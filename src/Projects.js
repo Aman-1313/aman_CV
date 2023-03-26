194272213d74e86cc5a1cac53c73956aaebe3238
Aman-1313 <amansainigahonia@gmail.com>
2023-03-26 15:31:08 -0500

    Projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import "./css/bootstrap.min.css";
-
 function Projects() {
+    const works = ['Abacus Analysis',
+    'Amrit Bani',
+    'LinkedIn Automation',
+    'GitHub Bug Prediction'];
+    const worksmap = {
+        'Abacus Analysis': "https://abacusanalysis.ca",
+        'Amrit Bani':"https://play.google.com/store/apps/details?id=com.Amrit",
+        'LinkedIn Automation':"",
+        'GitHub Bug Prediction':""
+    };
+    const worksimg = {
+        'Abacus Analysis': '/abacus.png',
+        'Amrit Bani': '/amrit.png',
+        'LinkedIn Automation':"",
+        'GitHub Bug Prediction':""
+    };
+      const [show, setShow] = useState("")
     return (
       <div class="container">
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/">Homepage</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -22,24 +39,28 @@ function Projects() {
                 </div>
             </nav>
         </div>
-        <div class="row row-lg-12">
+        <div class="row">
             <div class="col col-lg-4">
-                <ul class="list">
-                    <h2 className='intro'>Projects</h2>
-                    <li class="list-group-item mt-5">Abacus Analysis</li>
+                <ul>
+                <div className='container mb-5'><h2 className='list'>Projects</h2></div>
+                    {works.map( work => 
+                    <>
+                    <a class="nav-link" href= {worksmap[work]} >
+                        <button type="button" onMouseLeave={()=>setShow("")} onMouseEnter={()=>setShow(work)}  class="btn">
+                            <li class="list-group-item">{work}</li>
+                        </button>
+                    </a>
                     <hr/>
-                    <li class="list-group-item">Amrit Bani</li>
-                    <hr/>
-                    <li class="list-group-item">LinkedIn Automation</li>
-                    <hr/>
-                    <li class="list-group-item">GitHub Bug Prediction</li>
+                    </>)}
                 </ul>
             </div>
-            <div class="col col-lg-8">
-            <iframe style={{borderRadius:16}} src="https://abacusanalysis.ca" width="100%" height="100%">
-            <p>Your browser does not support iframes.</p>
-        </iframe>
-            </div>
+            {show&&<><div class="col-12 col-md-8">
+            <img className='fade-in-image2' src={process.env.PUBLIC_URL + worksimg[show]} style={{width:"100%", height:"100%", borderRadius:16}}  alt="project" />
+            {/* <iframe style={{borderRadius:16}} src={worksmap[show]} width="100%" height="100%">
+                <p>Your browser does not support iframes.</p>
+            </iframe> */}
+            </div></>}
+            
         </div>
        
         
